41f3f795f96ceaa70a8ff259b2c4bde9d7b59890
Darrin Speegle <speegled@slu.edu>
2021-01-21 13:58:45 -0600

    added data from ITSwR

---
+++ R/maine.R
@@ -0,0 +1,11 @@
+#' Maine unemployment data
+#'
+#' Monthly unemployment figures in Maine starting from January, 1996.
+#'
+#' @format A data frame with 128 observations of 1 variable
+#' \describe{
+#'   \item{unemploy}{Unemployment rate}
+#' }
+#'
+#' @source Introductory Time Series with R by Cowpertwait and Metcalfe
+"maine"
