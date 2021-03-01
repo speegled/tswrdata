254c33b8077377a2ddf0449ad0da6f1471683771
Darrin Speegle <speegled@slu.edu>
2021-03-01 12:15:25 -0600

    sim_data

---
+++ R/sim_data.R
@@ -0,0 +1,14 @@
+#' Simulated time series data
+#'
+#' The data sim_data_*, where * is an integer, are simulated time series of various types.
+#'
+#' @name sim_data
+#' @format Vectors with varying lengths
+#'
+#' @source Constructed by Darrin Speegle
+
+#' @rdname sim_data
+"sim_data_1"
+
+#' @rdname sim_data
+"sim_data_2"
